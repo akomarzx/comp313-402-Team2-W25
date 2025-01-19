21c23250760f2448d6858dd1aa89836e60a74320
Hung Nguyen <nguyenmanhhungpro15@gmail.com>
2025-01-19 17:40:14 -0500

    recipe list now as component

--- frontend/src/app/(root)/ai-rcmd/recipes/page.jsx
+++ frontend/src/app/(root)/ai-rcmd/recipes/page.jsx
@@ -3,11 +3,18 @@
 import React from "react";
 
 import { useSearchParams } from "next/navigation";
+import RecipesResult from "@/components/RecipesResult";
 const AIRecipies = () => {
   const searchParams = useSearchParams();
   const data = JSON.parse(searchParams.get("data") || "{}");
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      <div className="border-t-2">
+        <RecipesResult isSearching="" recipeCardData={data} />
+      </div>
+    </div>
+  );
 };
 
 export default AIRecipies;
