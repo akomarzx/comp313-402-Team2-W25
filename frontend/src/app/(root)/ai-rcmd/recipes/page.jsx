76d9efd911a604895014582c8429c1921a998a1c
Hung Nguyen <nguyenmanhhungpro15@gmail.com>
2025-01-19 17:58:54 -0500

    add suspense

--- frontend/src/app/(root)/ai-rcmd/recipes/page.jsx
+++ frontend/src/app/(root)/ai-rcmd/recipes/page.jsx
@@ -1,18 +1,21 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 
 import { useSearchParams } from "next/navigation";
 import RecipesResult from "@/components/RecipesResult";
+
 const AIRecipies = () => {
   const searchParams = useSearchParams();
   const data = JSON.parse(searchParams.get("data") || "{}");
 
   return (
     <div>
-      <div className="border-t-2">
-        <RecipesResult isSearching="" recipeCardData={data} />
-      </div>
+      <Suspense>
+        <div className="border-t-2">
+          <RecipesResult isSearching="" recipeCardData={data} />
+        </div>
+      </Suspense>
     </div>
   );
 };
