18d796c9abee2f9ded5a64a395bd2333741f0527
Ronald Ombao <ronaldjr.ombao@gmail.com>
2025-02-02 18:24:01 -0500

    CategoryController added.

---
+++ backend/src/main/java/org/group2/comp313/kitchen_companion/controller/CategoryController.java
@@ -0,0 +1,31 @@
+package org.group2.comp313.kitchen_companion.controller;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.group2.comp313.kitchen_companion.domain.projection.CategorySummary;
+import org.group2.comp313.kitchen_companion.dto.ApiResult;
+import org.group2.comp313.kitchen_companion.repository.CategoryRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+@SecurityRequirement(name = "Keycloak")
+@Tag(name = "Category API")
+public class CategoryController extends BaseController {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResult<List<CategorySummary>>> getAllCategories() {
+        return ResponseEntity.ok(new ApiResult<>("", this.categoryRepository.findAllBy()));
+    }
+}
--- backend/src/main/java/org/group2/comp313/kitchen_companion/repository/CategoryRepository.java
+++ backend/src/main/java/org/group2/comp313/kitchen_companion/repository/CategoryRepository.java
@@ -1,9 +1,13 @@
 package org.group2.comp313.kitchen_companion.repository;
 
 import org.group2.comp313.kitchen_companion.domain.Category;
+import org.group2.comp313.kitchen_companion.domain.projection.CategorySummary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer>, JpaSpecificationExecutor<Category> {
     Category findByLabel(String cleanedLabel);
+    List<CategorySummary> findAllBy();
 }
