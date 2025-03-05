655f7a00368ca6c81d75444797fc3110a878e416
Ronald Ombao <ronaldjr.ombao@gmail.com>
2025-03-05 09:02:06 -0500

    Fixed Bug in ratings api after table change.

--- backend/src/main/java/org/group2/comp313/kitchen_companion/repository/RatingCalculatedRepository.java
+++ backend/src/main/java/org/group2/comp313/kitchen_companion/repository/RatingCalculatedRepository.java
@@ -1,7 +1,11 @@
 package org.group2.comp313.kitchen_companion.repository;
 
+import jakarta.validation.constraints.NotNull;
 import org.group2.comp313.kitchen_companion.domain.RatingCalculated;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RatingCalculatedRepository extends JpaRepository<RatingCalculated, Integer> {
+    Optional<RatingCalculated> findByRecipe(@NotNull Integer recipe);
 }
